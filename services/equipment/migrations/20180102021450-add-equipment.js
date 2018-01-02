803af1a67ac7dd08c76eb248491f5ee1d723667c
TienThanh995 <ntt995@github.com>
2018-01-02 15:03:11 +0700

    using ES6 standard

--- services/equipment/migrations/20180102021450-add-equipment.js
+++ services/equipment/migrations/20180102021450-add-equipment.js
@@ -6,13 +6,13 @@ let seed;
   * We receive the dbmigrate dependency from dbmigrate initially.
   * This enables us to not have to rely on NODE_PATH.
   */
-exports.setup = function(options, seedLink) {
+exports.setup = (options, seedLink) => {
   dbm = options.dbmigrate;
   type = dbm.dataType;
   seed = seedLink;
 };
 
-exports.up = function(db, cb) {
+exports.up = (db, cb) => {
   db.createTable('equipment', {
     id: {type: 'int', notNull: true, primaryKey: true, autoIncrement: true},
     type: {type: 'string', notNull: true},
@@ -21,7 +21,7 @@ exports.up = function(db, cb) {
   }, cb);
 };
 
-exports.down = function(db, cb) {
+exports.down = (db, cb) => {
   return db.dropTable('equipment', cb);
 };
 
